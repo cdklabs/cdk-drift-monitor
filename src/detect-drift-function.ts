85b6ce2da08006702acbfaabcd77d1cb94ba251e
corymhall <43035978+corymhall@users.noreply.github.com>
2022-10-26 15:49:01 +0000

    chore: update deps
    The upgrade-main workflow is failing due to using node12. Running the
    upgrade manually and creating this PR which should fix it going forward.

--- src/detect-drift-function.ts
+++ src/detect-drift-function.ts
@@ -17,7 +17,7 @@ export class DetectDriftFunction extends lambda.Function {
     super(scope, id, {
       description: 'src/detect-drift.lambda.ts',
       ...props,
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: new lambda.Runtime('nodejs14.x', lambda.RuntimeFamily.NODEJS),
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, '../assets/detect-drift.lambda')),
     });
